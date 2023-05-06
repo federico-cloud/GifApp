0bb2fedf2058318d4f8af0863d75af0d9705d19c
Federico Galan <fegv.2@hotmail.com.ar>
2023-05-06 09:49:12 -0300

    First commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,8 @@
+
+export const GiftExpertApp = () => {
+  return (
+    <>
+        GiftExpertApp
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import { GiftExpertApp } from './GiftExpertApp'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GiftExpertApp />
+  </React.StrictMode>,
+)
