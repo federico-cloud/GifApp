4a8968bc9df57e6d8804c6d429eb49aa7e748fc0
Federico Galan <fegv.2@hotmail.com.ar>
2023-05-06 21:46:15 -0300

    Component: The name was corrected and its structure was modified

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { AddCategory } from "./components/addCategory"
+
+export const GiftExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+  
+  const handleAddCategory = () => {
+    setCategories([...categories]);
+  }
+ 
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory/>
+      <button onClick={handleAddCategory}>Add categorie</button>
+      <ol>
+        {
+          categories.map(categorie => {
+            return <li key={categorie}>{categorie}</li>
+          })
+        }
+      </ol>
+    </>
+  )
+}
--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
+import { AddCategory } from "./components/addCategory"
 
 export const GiftExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+  
+  const handleAddCategory = () => {
+    setCategories([...categories]);
+  }
+ 
   return (
     <>
-      GiftExpertApp
+      <h1>GiftExpertApp</h1>
+
+      <AddCategory/>
+      <button onClick={handleAddCategory}>Add categorie</button>
+      <ol>
+        {
+          categories.map(categorie => {
+            return <li key={categorie}>{categorie}</li>
+          })
+        }
+      </ol>
     </>
   )
 }
