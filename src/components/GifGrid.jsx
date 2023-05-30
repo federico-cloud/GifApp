d95997a172c0e5be012a1e609901afe2593d8e8e
Federico Galan <fegv.2@hotmail.com.ar>
2023-05-30 17:02:06 -0300

    Component: GifGrid was modified to display error if the images is empty

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,25 +1,61 @@
 import { useEffect, useState } from "react";
+import { GridItem } from "./GridItem";
 import { getGifs } from "../helpers/getGifs";
+import { Box, CircularProgress } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 export const GifGrid = ({category}) => {
 
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);    
+  const [isLoading, setIsLoading] = useState(true);
+  const [isClosed, setIsClosed] = useState(false);
 
-  const getImages = async(category) => {
+  const getImages = async (category) => {
     const images = await getGifs(category);
+    setIsLoading(false);
     setImages(images);
   }
 
   useEffect(() => {
-    getImages(category);
-  }, [])
+    if (!isClosed) {
+      getImages(category);
+    }
+  }, [category, isClosed])
+
+  const handleClose = () => {
+    setIsClosed(true);
+  }
 
   return (
     <>
-      <h3>{category}</h3>
-      <ol>
-        {images.map(({id, title}) => <li key={id}>{title}</li>)}
-      </ol>
+      {!isClosed && (
+        <div className="center">
+          <h3>{category}</h3>
+          <CloseIcon className="close" fontSize="large" onClick={handleClose} />
+        </div>
+      )}
+      {!isClosed && (
+        <div>
+          {isLoading ? (
+            <Box 
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              height="100vh"
+            >
+              <CircularProgress size={80} thickness={5} />
+            </Box>
+          ) : images.length === 0 ? (
+            <p className="error-card">Images weren't found for the category {category}.</p>
+          ) : (
+            <div className="card-grid">
+              {images.map(({ id, title, url }) => (
+                <GridItem key={id} title={title} url={url} />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </>
-  )
+  );
 }
