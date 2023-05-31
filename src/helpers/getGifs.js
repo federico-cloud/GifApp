5d559e5d4cd3ef5dd3e3c20324bb430965942e18
Federico Galan <fegv.2@hotmail.com.ar>
2023-05-31 10:24:08 -0300

    Helper: getGif was modified with minor changes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,15 +1,15 @@
 export const getGifs = async(category) => {
-    
-    const url    = `https://api.giphy.com/v1/gifs/search?api_key=FQNhGq9VGKl9uLcBgROf6bKArM2jKtTR&q=${category}`
-    const resp   = await fetch(url);
-    const {data} = await resp.json();
   
-    const gifs = data.map(img => ({
-      id: img.id,
-      title: img.title,
-      url: img.images.downsized_medium.url
-    }))
+  const url    = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${category}`;
+  const resp   = await fetch(url);
+  const {data} = await resp.json();
   
-    return gifs
+  const gifs = data.map(img => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url
+  }))
+
+  return gifs
 }
   
