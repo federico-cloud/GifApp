a865c478dc1b2f1c3522ee06ddc98be30777b4e0
Federico Galan <fegv.2@hotmail.com.ar>
2023-05-31 06:50:42 -0300

    Component: The component AddCategory was modified wrong behavior was corrected and imports was optimized

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const AddCategory = ({onNewCategory}) => {
 
     const [inputValue, setInputValue] = useState('');
     const [showError, setShowError] = useState(false);
+    const [hasInteracted, setHasInteracted] = useState(false);
 
+    useEffect(() => {
+        if (hasInteracted) {
+          setShowError(false);
+          if (inputValue.length === 0 && showError) {
+            setShowError(true);
+          }
+        }
+      }, [inputValue, hasInteracted]);
+    
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          setShowError(true);
+        }
+    };
         
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
+        if (inputValue.length + 1 !== 0) {
+            setShowError(false);
+        }
     }
     
     const handleSubmit = (event) => {
@@ -29,13 +47,10 @@ export const AddCategory = ({onNewCategory}) => {
                 placeholder="Search gifs of ..."
                 value={inputValue}
                 onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
             />
             <div className='error-msg'>
-                {
-                    showError 
-                    ? <span>The text musn't be empty</span>
-                    : false
-                }
+                { showError ? <span>The text musn't be empty</span> : false }
             </div>
         </form>  
     )
